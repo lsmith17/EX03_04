09f7c129cbdab366c12c8d963518d7fc3e06d058
lsmith17 <lsmith17@my.whitworth.edu>
2014-05-28 19:41:07 -0700

    Fixed Code
    Almost there

--- EX03_04/EX03_04/String.cpp
+++ EX03_04/EX03_04/String.cpp
@@ -1,6 +1,38 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main(){
+string sort(string &s){
+	for (int i = s.length() - 1; i >= 1; i--)
+	{
+		char currentMax = s[0];
+		int currentMaxPlace = 0;
 
+		for (int j = 1; j <= i; j++)
+		{
+			if (currentMax < s[j])
+			{
+				currentMax = s[j];
+				currentMaxPlace = j;
+			}
+		}
+
+		if (currentMaxPlace != i)
+		{
+			s[currentMaxPlace] = s[i];
+			s[i] = currentMax;
+		}
+	}
+
+	return s;
+}
+
+int main(){
+	string s;
+	cout << "Enter a string s: ";
+	cin >> s;
+
+	cout << "The sorted string is " << sort(s) << endl;
+
+	return 0;
 }
